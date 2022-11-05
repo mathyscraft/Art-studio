f042566e8a0af5e5d9f43de8fb8a3f5397d2bf7a
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2022-11-05 13:25:05 +0100

    adaptation mobile

--- app.js
+++ app.js
@@ -2,27 +2,46 @@ let mouseX;
 let mouseY;
 let oldMouseX;
 let oldMouseY;
+// Détection pointeur pc
 document.onmousemove = (e) => {
     oldMouseX = mouseX;
     oldMouseY = mouseY;
     mouseX = e.clientX + window.pageXOffset;
     mouseY = e.clientY + window.pageYOffset;
-    draw()
+    if (mouseDown === true) {
+        mouseUpped = false; // inutile sur pc donc faux par défaut
+        draw()
+    }
 }
 
-let mouseDown = false;
+let mouseDown = false; // Permet, sur pc, de dire sur la souris est enfoncée
 document.onmousedown = () => {
     mouseDown = true;
 }
-
 document.onmouseup = () => {
     mouseDown = false;
 }
 
+// Détection pointeur mobile
+let mouseUpped = false; // Permet, sur mobile, de dire si le doigt vient d'être levé
+document.ontouchmove = (e) => {
+    if (mouseUpped === false) {
+        oldMouseX = mouseX;
+        oldMouseY = mouseY;
+    }
+    mouseX = e.touches[0].clientX + window.pageXOffset;
+    mouseY = e.touches[0].clientY + window.pageYOffset;
+    draw()
+    mouseUpped = false;
+}
+document.ontouchend = () => {
+    mouseUpped = true;
+}
 
+// Dessin
 const canvas = document.querySelector('canvas');
-canvas.width = document.documentElement.clientWidth - 10;
-canvas.height = document.documentElement.clientHeight - 10;
+canvas.width = document.documentElement.clientWidth - 20;
+canvas.height = document.documentElement.clientHeight - 20;
 
 const ctx = document.querySelector('canvas').getContext("2d");
 const palette = document.querySelector('input[type="color"]');
@@ -34,20 +53,20 @@ changeWeight();
 inputWeight.addEventListener('change', changeWeight);
 
 function draw() {
-    if (mouseDown === true) {
-        ctx.beginPath();
-        ctx.lineWidth = weight;
-        ctx.arc(mouseX, mouseY, weight, 0, 100);
-    // Trace des cercles entre les deux positions de la souris connues pour eviter les espaces lacunaires
+    ctx.beginPath();
+    ctx.lineWidth = weight;
+    ctx.arc(mouseX, mouseY, weight, 0, 100);
+// Trace des cercles entre les deux positions de la souris connues pour eviter les espaces lacunaires
+    if (mouseUpped === false) { // Si sur mobile, le doigt ne vient pas d'être levé ; faux par defaut sur pc
         for (i = 0.01; i < 1; i = i + 0.01) {
             ctx.arc((oldMouseX-mouseX)*i+mouseX,(oldMouseY-mouseY)*i+mouseY, weight, 0, 100);
         }
-        
-        // ctx.moveTo(oldMouseX, oldMouseY);
-        // ctx.lineTo(mouseX, mouseY)
-        // ctx.stroke();
-        ctx.fill();
     }
+    
+    // ctx.moveTo(oldMouseX, oldMouseY);
+    // ctx.lineTo(mouseX, mouseY)
+    // ctx.stroke();
+    ctx.fill();
 }
 
 function changeColor() {
@@ -61,4 +80,3 @@ function changeWeight() {
         weight = 0.5;
     }
 }
-
